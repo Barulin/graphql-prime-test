145b02a1015c7fb8ba6e29364d11a2a37fb795b5
Dmitriy Barulin <kpot90@rambler.ru>
2018-10-25 02:05:00 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'graphql-prime-test';
+
+export class AppComponent implements OnInit {
+  rates: SelectItem[] = [];
+  loadingrates = true;
+  errorrates: any;
+  countries: SelectItem[] = [];
+  loadingcountries = true;
+  errorcountries: any;
+  selectedValue = 'val2';
+  selectedRate: SelectItem | null = null;
+  selection: SelectItem[];
+  selectedCurrency = 'RUB';
+
+  constructor(private apollo: Apollo) {
+    this.selection = [
+      {label: 'Select City', value: null},
+      {label: 'New York', value: {id: 1, name: 'New York', code: 'NY'}},
+      {label: 'Rome', value: {id:  2, name: 'Rome', code: 'RM'}},
+      {label: 'London', value: {id: 3, name: 'London', code: 'LDN'}}
+    ];
+  }
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            rates(currency: "${this.selectedCurrency}") {
+              currency
+              rate
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe(result => {
+        if (result.data && result.data.rates) {
+          // this.rates
+        }
+        console.log(result)
+        this.rates = result.data.rates;
+        this.loadingrates = result.loading;
+        this.errorrates = result.errors;
+
+      });
+
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            countries {
+              native
+              emoji
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe(result => {
+        this.countries = result.data.countries;
+        this.loadingcountries = result.loading;
+        this.errorcountries = result.errors;
+      });
+  }
+
+  changeCurrency(newCurrency: string) {
+    this.selectedCurrency = newCurrency;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/primeng';
+import { ButtonModule } from 'primeng/button';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { GraphQLModule} from './graphql.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +21,29 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    DropdownModule,
+    ButtonModule,
+    RadioButtonModule,
+    FormsModule,
+    HttpClientModule,
+    ApolloModule,
+    GraphQLModule,
+    HttpLinkModule
   ],
-  providers: [],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory(httpLink: HttpLink) {
+      return {
+        cache: new InMemoryCache(),
+        link: httpLink.create({
+          uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql'
+        })
+      }
+    },
+    deps: [HttpLink]
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
